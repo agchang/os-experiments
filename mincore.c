9cd1bcb060b657bd622f4864b2a68b584d409af7
Andrew Chang <andrewgchang@gmail.com>
2026-03-04 12:56:57 -0800

    mincore experiment

---
+++ mincore.c
@@ -0,0 +1,42 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+void print_mincore(void *ptr, size_t length, unsigned char *vec, size_t vec_size) {
+  int status = mincore(ptr, 4096, vec);
+  printf("status: %d\n", status);
+  if (status == -1) {
+    printf("%d\n", errno);
+    printf("%s\n", strerror(errno));
+  }
+  for (int i = 0; i < vec_size; i++) {
+    printf("%d\n", vec[i]);
+  }
+}
+
+int main() {
+  int x = 0;
+  size_t vec_size = (4096+4095)/4096; // 1
+  printf("vec_size: %ld\n", vec_size);
+  char vec[vec_size];
+  uintptr_t addr = (uintptr_t)&x;
+  addr = (addr/4096) * 4096;
+  void* ptr = (void*)addr;
+  print_mincore(ptr, 4096, vec, vec_size);
+
+  // Now try a mmap file
+  int fd = open("output.bin", O_RDONLY);
+  void* fptr = mmap(NULL, 4096*2, PROT_READ, MAP_PRIVATE, fd, 0);
+  // interesting part - this will return 1 if the os has it in the page cache, for example when I first created the file
+  // run echo 3 > /proc/sys/vm/drop_caches to flush the page cache
+  print_mincore(fptr, 4096, vec, vec_size);
+  // access it
+  printf("%d", *(char*)fptr);
+  // now if you run this program again, it's in the page cache!
+
+  return 0;
+}
