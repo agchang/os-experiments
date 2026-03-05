0c13fcdcd0a2aa23c83caa24a88c56a6c346f036
Andrew Chang <andrewgchang@gmail.com>
2026-03-05 13:06:12 -0800

    measure a warm read

--- mincore.c
+++ mincore.c
@@ -5,6 +5,13 @@
 #include <stdint.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <time.h>
+
+static double now_ns(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1e9 + ts.tv_nsec;
+}
 
 void print_mincore(void *ptr, size_t length, unsigned char *vec, size_t vec_size) {
   int status = mincore(ptr, 4096, vec);
@@ -35,8 +42,19 @@ int main() {
   // run echo 3 > /proc/sys/vm/drop_caches to flush the page cache
   print_mincore(fptr, 4096, vec, vec_size);
   // access it
-  printf("%d", *(char*)fptr);
+  double start_ts = now_ns();
+  volatile char val = *(char*)fptr;
+  double elapsed = now_ns() - start_ts;
   // now if you run this program again, it's in the page cache!
+  printf("%d\n", val);
+  printf("%f\n", elapsed);
+  
+  // access time once it's in the page table
+  start_ts = now_ns();
+  val = *(volatile char*)fptr;
+  elapsed = now_ns() - start_ts;
+  printf("%d\n", val);
+  printf("%f\n", elapsed);
 
   return 0;
 }
